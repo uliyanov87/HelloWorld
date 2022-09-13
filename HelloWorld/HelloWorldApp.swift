37f6b5ebeb829a3c130c39309211b59c85e96217
Ulyanov Nikita <uliyanov87@icloud.com>
2022-09-13 17:45:56 +0300

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Nikita Uliyanov on 13.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
